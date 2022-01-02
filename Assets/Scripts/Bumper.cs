076c66790f59f209c75d39a69613201b83ef51e0
Eren Unsal <aaronunsal@gmail.com>
2022-01-03 00:59:30 +0300

    Bumper script added

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    [SerializeField] public int bumperNumber;
+    [SerializeField] float speed = 5f;
+    Rigidbody2D rb;
+
+    string verticalControls;
+    float movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        AssignCorrectControls();
+    }
+
+    void Update()
+    {
+        ReadMovement();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void AssignCorrectControls()
+    {
+        verticalControls = "Vertical" + bumperNumber;
+    }
+
+    private void ReadMovement()
+    {
+        movement = Input.GetAxisRaw(verticalControls);
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+}
